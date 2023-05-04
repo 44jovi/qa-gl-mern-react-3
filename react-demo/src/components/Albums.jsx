6f91095381d7989fcc0c5bc6bcf4756ebf7bcc0c
44jovi <kwong.jovi@gmail.com>
2023-05-04 10:26:30 +0100

    update(Albums) - render all data rows

--- react-demo/src/components/Albums.jsx
+++ react-demo/src/components/Albums.jsx
@@ -1,12 +1,33 @@
 import albums from "../resources/albums.json";
-function Albums() {
+const Albums = () => {
+  const printData = () => {
+    return albums.map((a, index) => {
+      const { userId, id, title } = a;
+      console.log(a);
+      return (
+        <tr key={index}>
+          <td>{userId}</td>
+          <td>{id}</td>
+          <td>{title}</td>
+        </tr>
+      );
+    });
+  };
+
   return (
     <>
-      {albums.map((a, index) => {
-        return a.userId;
-      })}
+      <table>
+        <thead>
+          <tr>
+            <th>userId</th>
+            <th>id</th>
+            <th>title</th>
+          </tr>
+        </thead>
+        <tbody>{printData()}</tbody>
+      </table>
     </>
   );
-}
+};
 
 export default Albums;
