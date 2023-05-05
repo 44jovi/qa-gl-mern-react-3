b58d2bf7ff438304bc5ac2019bd8f7ad6a040814
jovi <kwong.jovi@gmail.com>
2023-05-06 00:10:11 +0100

    update(OMDB): print data only if API data exists

--- react-demo/src/components/omdb/OMDB.jsx
+++ react-demo/src/components/omdb/OMDB.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Input, Button, Table } from "reactstrap";
+import { Input, Button, Table, Spinner } from "reactstrap";
+
 const OMDB = () => {
   const [movieTitle, setMovieTitle] = useState("hobbit");
   const [movieData, setMovieData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const getData = () => {
     const url = "http://www.omdbapi.com/?apikey=";
@@ -12,13 +14,16 @@ const OMDB = () => {
       .get(url + APIkey + "t=" + movieTitle)
       .then((data) => {
         setMovieData(data.data);
+        setIsLoaded(true);
         console.log("DATA FROM API:");
         console.log(data);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
-  //   Only needed if you want to display a movie before user input
+  // Only needed if you want to display a movie before user input
   useEffect(() => {
     setTimeout(() => {
       getData();
@@ -26,55 +31,74 @@ const OMDB = () => {
   }, []);
 
   const printData = () => {
-    const { Year, Genre, Rated, Director, Plot, Ratings } = movieData;
-
-    return (
-      <>
-        <tr>
-          <td>Year</td>
-          <td>{Year}</td>
-        </tr>
-        <tr>
-          <td>Genre</td>
-          <td>{Genre}</td>
-        </tr>
-        <tr>
-          <td>Rated</td>
-          <td>{Rated}</td>
-        </tr>
-        <tr>
-          <td>Director</td>
-          <td>{Director}</td>
-        </tr>
-        <tr>
-          <td>Plot</td>
-          <td>{Plot}</td>
-        </tr>
-        <tr>
-          <td>Ratings (WIP)</td>
-          <td></td>
-        </tr>
-      </>
-    );
+    if (isLoaded === true) {
+      const { Year, Genre, Rated, Director, Actors, Plot, Ratings } = movieData;
+      return (
+        <>
+          <tr>
+            <td>Year</td>
+            <td>{Year}</td>
+          </tr>
+          <tr>
+            <td>Genre</td>
+            <td>{Genre}</td>
+          </tr>
+          <tr>
+            <td>Rated</td>
+            <td>{Rated}</td>
+          </tr>
+          <tr>
+            <td>Director</td>
+            <td>{Director}</td>
+          </tr>
+          <tr>
+            <td>Actors</td>
+            <td>{Actors}</td>
+          </tr>
+          <tr>
+            <td>Plot</td>
+            <td>{Plot}</td>
+          </tr>
+          <tr>
+            <td>Ratings</td>
+            <td>
+              {Ratings.map((r) => {
+                return <p>{r.Source + ": " + r.Value}</p>;
+              })}
+            </td>
+          </tr>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h1>LOADING...</h1>
+          <Spinner
+            color="primary"
+            style={{
+              height: "10rem",
+              width: "10rem",
+            }}
+            type="grow"
+          />
+        </>
+      );
+    }
   };
 
   return (
     <>
       <h1>{movieData.Title}</h1>
-      <img src={movieData.Poster} alt={movieData.Title} width="25%"></img>
-      <Table dark style={{ width: "25%" }}>
+      <img src={movieData.Poster} alt={movieData.Title}></img>
+      <Table dark>
         <thead>
           <th></th>
           <th></th>
         </thead>
         <tbody>{printData()}</tbody>
       </Table>
-      <Input
-        type="text"
-        onChange={(e) => setMovieTitle(e.target.value)}
-        style={{ width: "25%" }}
-      />
-      <Button color="success" onClick={getData} style={{ width: "25%" }}>
+      <Input type="text" onChange={(e) => setMovieTitle(e.target.value)} />
+      <Button color="success" onClick={getData}>
         GO
       </Button>
     </>
